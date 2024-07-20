b5016d664802f915c2f7d9fcddb1080bb7457bf8
Rodrigo Goncalves <rodrigogoncalves2408@gmail.com>
2024-07-20 10:20:59 -0300

    De 100 para 50 números

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Lista de números já sorteados para não gerar repetição
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -15,7 +15,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
 
 }
 
